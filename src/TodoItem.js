cefeb376a03a588123b751f0e3054e2067bce009
Jerry Shi <szy0syz@gmail.com>
2018-08-28 17:26:19 +0800

    Update 第二版：组件化+组件通信

---
+++ src/TodoItem.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+class TodoItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    // 那么问题来了，子组件是不能修改父组件的数据。所以只能让子组件调用父组件修改数据的方法来修改父组件数据
+    console.log(this.props.index);
+    //
+    this.props.deleteItem(this.props.index);
+    // --> 上面的调用好比在调用这个函数 this.handleItemDelete，但这个方法是父组件的，子组件拿不到啊
+    // --> 所以我们需要在父组件里传递handleDeleteItem时就把this强制绑定成父组件的this
+  }
+
+  render() {
+    return <li onClick={this.handleClick}>{this.props.itemValue}</li>;
+  }
+}
+
+export default TodoItem;
--- src/Todolist.js
+++ src/Todolist.js
@@ -1,4 +1,5 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import TodoItem from './TodoItem'
 
 class Todolist extends Component {
   constructor(props) {
@@ -8,38 +9,48 @@ class Todolist extends Component {
       inputValue: "",
       list: []
     };
+
+    this.handleItemDelete = this.handleItemDelete.bind(this)
   }
 
   handleBtnClick() {
     this.setState({
-      inputValue: '',
+      inputValue: "",
       list: [...this.state.list, this.state.inputValue]
-    })
+    });
   }
 
   handleInputChange(e) {
     this.setState({
       inputValue: e.target.value
-    })
+    });
   }
 
   handleItemDelete(index) {
     // immutable
-    const list = [...this.state.list]   // 必须做个复本重新修改
-    list.splice(index, 1)
+    const list = [...this.state.list]; // 必须做个复本重新修改
+    list.splice(index, 1);
     this.setState({
       list
-    })
+    });
+  }
+
+  handleInputKeyUp(e) {
+    if (e.keyCode === 13) {
+      this.handleBtnClick();
+    }
   }
 
   render() {
     return (
-      <div style={{ marginTop: "6px" }}>
-        <div>
-          <label>ItemName：</label>
-          <input 
-            value={this.state.inputValue} 
+      <Fragment>
+        <div style={{ marginTop: "6px" }}>
+          <label htmlFor="InutArea">ItemName：</label>
+          <input
+            id="InutArea"
+            value={this.state.inputValue}
             onChange={this.handleInputChange.bind(this)}
+            onKeyDown={this.handleInputKeyUp.bind(this)}
           />
           <button
             onClick={this.handleBtnClick.bind(this)}
@@ -53,11 +64,24 @@ class Todolist extends Component {
         <div>
           <ul>
             {this.state.list.map((item, index) => {
-              return <li key={index} onClick={this.handleItemDelete.bind(this, index)}>{item}</li>;
+              return (
+                <TodoItem 
+                  key={index} 
+                  itemValue={item} 
+                  index={index}
+                  deleteItem={this.handleItemDelete}
+                ></TodoItem>
+                // <li
+                //   key={index}
+                //   onClick={this.handleItemDelete.bind(this, index)}
+                //   dangerouslySetInnerHTML={{__html: item}}
+                // >
+                // </li>
+              );
             })}
           </ul>
         </div>
-      </div>
+      </Fragment>
     );
   }
 }
